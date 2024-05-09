8bf2024985b08b2508266cb419697d99d67b60ca
NixonDávila <davilanixon8@gmail.com>
2024-05-09 15:42:53 -0600

    js de la calcu de propina

--- calcu.js
+++ calcu.js
@@ -77,6 +77,7 @@ function añadirNumeros(e){
 
 
 
+
 //Funcion para calcular la propina
 function calularPropina()
 {
@@ -84,6 +85,33 @@ function calularPropina()
 var totalCompra = document.getElementById('box').value;
 var porcentajePropina = document.getElementById('box2').value;
 var totalPersonas = document.getElementById('box3').value;
+
+//formulas
+var porcentaje = porcentajePropina/100;
+var resultado1 = totalCompra*porcentaje;
+var resultado2 = resultado1/totalPersonas;
+
+
+
+if (totalCompra=="") {
+	
+	document.getElementById('r3').innerHTML = "INTRODUZCA TTODOS LOS DATOS";
+	document.getElementById('r1').innerHTML = ":(";
+	document.getElementById('r2').innerHTML = ":(";
+}
+
+else{
+
+	//respuestas
+
+	document.getElementById('r1').innerHTML = resultado1.toFixed(2);
+	
+	document.getElementById('r2').innerHTML = resultado2.toFixed(2);
+
+
+
+}
+
 
 
 }
